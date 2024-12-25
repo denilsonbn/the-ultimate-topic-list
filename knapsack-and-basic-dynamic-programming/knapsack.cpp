186d11ca9d782814db7a1a0e4117e9998f47c594
Jose Nilson Bernardo <denilsonnunez098@gmail.com>
2024-12-24 21:28:39 -0300

    up

--- knapsack-and-basic-dynamic-programming/knapsack.cpp
+++ knapsack-and-basic-dynamic-programming/knapsack.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+#include <limits>
+
+#define int long long
 
 using namespace std;
 
+int const MAXN = INT64_MAX;
+
 vector<pair<int, int>> nums;
-vector<int> dp;
+vector<vector<int>> dp;
 int n, w;
 
 int knap(int i, int curr) {
     if (i == n) return 0; 
 
-    if (dp[curr] != -1) return dp[w];
+    if (dp[i][curr] != MAXN) return dp[i][curr];//melhor com peso curr
 
     int item = 0;
+    int no_item = 0;
 
     if (curr + nums[i].first <= w) {
         item = knap(i+1, curr+nums[i].first) + nums[i].second;
     }
-    int no_item = 0;
-    no_item = knap(i+1, w);
-    dp[curr] = max(item, no_item);
+    no_item = knap(i+1, curr);
+    dp[i][curr] = max(item, no_item);
 
-    return dp[curr];
+    return dp[i][curr];
 
 }
 
-int main() {
+int32_t main() {
     cin >> n >> w;
-    dp.assign(w+1, -1);
+    dp.assign(n, vector<int>(w+1, MAXN));
 
     for (int i=0; i<n; i++) {
         int a, b;
@@ -37,9 +42,6 @@ int main() {
     }
 
     cout << knap(0, 0) << "\n";
-
-    for (auto e:dp) cout << e << " ";
-
 
     return 0;
 }
