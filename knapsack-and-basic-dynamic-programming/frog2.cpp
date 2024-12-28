5473d93d7fa139974a2a9f3fa5f5d81566ede454
Jose Nilson Bernardo <denilsonnunez098@gmail.com>
2024-12-28 00:16:37 -0300

    up

---
+++ knapsack-and-basic-dynamic-programming/frog2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n{0};
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for (int i=0; i<n; i++) cin >> nums[i];
+
+    
+
+
+    return 0;
+}
