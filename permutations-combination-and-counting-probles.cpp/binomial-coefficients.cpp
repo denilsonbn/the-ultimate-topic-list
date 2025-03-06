2d051a850cff094ff3b59c82c44d9cbbbd7ce5f6
Denilson <denilsonnunez098@gmail.com>
2025-03-05 21:05:50 -0300

    binomial-coefficients

--- permutations-combination-and-counting-probles.cpp/binomial-coefficients.cpp
+++ permutations-combination-and-counting-probles.cpp/binomial-coefficients.cpp
@@ -1,27 +1,50 @@
 #include <iostream>
 #include <vector>
 
+#define int long long
+
 using namespace std;
 
-const int MAXN = 1e4, MOD = 1e9+7;
+const int MOD = 1e9+7, MAXN = 1e6;
 
-vector<vector<int>> dp(MAXN + 1, vector<int>(MAXN + 1, 0));
-vector<bool> vis(MAXN + 1, 0);
+int fat[MAXN+2], inv[MAXN+2];
 
-long long bc(int n, int k) {
-    if (n == k || k == 0) return 1;
-    if (k == 1) return n;
+int power(int base, int exp) {
+    int ans = 1;
 
-    if (dp[n][k]) return dp[n][k];
-
-    return dp[n][k] = (bc(n-1, k-1) % MOD + bc(n-1, k) % MOD) % MOD;
-
+    while (exp > 0) {
+        if (exp & 1) ans = (base * ans) % MOD;
+        base = (base * base) % MOD;
+        exp >>= 1;
+    }
+    return ans;
 }
 
-int main() {
+void precompute() {
+    fat[0] = fat[1] = 1;
+
+    for (int i = 2; i <= MAXN; i++) {
+        fat[i] = (fat[i-1] % MOD * i % MOD) % MOD;
+    }
+
+    inv[MAXN] = power(fat[MAXN], MOD-2);
+
+    for (int i = MAXN - 1; i >= 0; i--) {
+        inv[i] = (inv[i+1] * (i+1)) % MOD;
+    }
+}
+
+int bc(int n, int k) {
+    return (fat[n] % MOD * inv[k] % MOD * inv[n-k] % MOD) % MOD;
+}
+
+
+int32_t main() {
     int q;
     cin >> q;
 
+    precompute();
+ 
     while (q > 0) {
         int n, k;
         cin >> n >> k;
